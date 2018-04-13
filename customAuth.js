bae87d91cb6f2d01efa83f556d626806a37e252e
EricJergensen <gericjergensen@gmail.com>
2018-04-13 15:54:21 -0500

    Add files via upload

--- customAuth.js
+++ customAuth.js
@@ -3,7 +3,6 @@ var domain = "default";
 
 function getPrincipal(request) {
   if ("SPNEGO".equals(request.getAuthType())) {
-    logger.info(request.getAuthType());
     var userName = request.getRemoteUser();
     logger.info(userName);
     var princ = request.getUserPrincipal();
@@ -12,26 +11,12 @@ function getPrincipal(request) {
     logger.info(user);
     if (user == null) {
       logger.info('creating user: ' + userName + '@' + domain);
-      var resolver = getAccountResolver(userName, domain);
-      logger.info('got resolver: ' + resolver);
-      var pair = resolver.getAdapterByUser(userName, domain);
-      logger.info('got pair: ' + pair);
+      var pair = accountResolver.getAdapterByUser(userName, domain);
       user = pair.getKey().addUser(new AccountUserDO(userName,null,{},"embedded",null,null), domain);
-      logger.info('added user: ' + user);
-      logger.info('added user/un: ' + user.getUserName());
-      logger.info('added user/dn: ' + user.getDomainName());
       pair.getKey().addUserToGroup(user, pair.getValue());
-      logger.info('user added to group');
     }
     logger.info(user);
     return user;
-  }
-  return null;
-}
-
-function getAccountResolver(userName, domainName) {
-  for each (var r in accountService.getAccountResolvers()) {
-    if (r.getAdapterByUser(userName, domainName) != null) return r;
   }
   return null;
 }
